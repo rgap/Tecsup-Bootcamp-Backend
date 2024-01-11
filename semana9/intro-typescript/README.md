170a5780bc887f07b8d33f4770d4dde42f9964a6
rgap <r.guzmanap@gmail.com>
2024-01-10 21:38:08 -0500

    typescript

---
+++ semana9/intro-typescript/README.md
@@ -0,0 +1,19 @@
+## Initialize
+
+```
+npm init
+```
+
+## Typescript
+
+```
+npm install typescript -D
+npx tsc
+
+Tipado para node js
+npm install @types/node -D
+npx tsc index.ts
+
+Esto crea la carpeta del output
+npx tsc
+```
---
+++ semana9/intro-typescript/index-old.ts
@@ -0,0 +1,73 @@
+const nombre: string = "Linder";
+const edad: number = 99;
+const esMayor: boolean = true;
+// nombre = 10
+
+function sumar(n1: number, n2: number): number {
+  return n1 + n2;
+}
+
+console.log(sumar(10, 1));
+
+if (esMayor) {
+  console.log(`Hola me llamo ${nombre} y tengo ${edad} años`);
+} else {
+  console.log(`Te falta crecer`);
+}
+
+// INTERFACE
+interface User {
+  name: string;
+  lastname: string;
+  email: string;
+  password: string;
+}
+
+interface UserOptionalAddress {
+  name: string;
+  lastname: string;
+  email: string;
+  password: string;
+  address: string;
+}
+
+const user: User = {
+  name: "Rel",
+  lastname: "Guzman",
+  email: "rgap@gmail.com",
+  password: "1312312",
+  address: "av mi casa",
+};
+
+const user2: UserOptionalAddress = {
+  name: "Rel",
+  lastname: "Guzman",
+  email: "rgap@gmail.com",
+  password: "1312312",
+  address: "av mi casa",
+};
+
+const users: User[] = [];
+
+function insertUser(user: User): void {
+  users.push(user);
+}
+
+function getNameFromUser(user: User): string {
+  return `${user.name} ${user.lastname}`;
+}
+
+// type
+type Status = "created" | "on-hold" | "in-progress" | "done";
+
+interface Task {
+  title: string;
+  date: Date;
+  status: Status;
+}
+
+const task: Task = {
+  title: "Pagar cuentas",
+  date: new Date(),
+  status: "",
+};
